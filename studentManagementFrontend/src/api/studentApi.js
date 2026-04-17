593c8987734d039217cd1aabf5b37a93e61ecf8a
gaurav-kuriyal <Gaurav.Kuriyal@coforge.com>
2026-04-17 06:39:58 +0000

    docker push

--- studentManagementFrontend/src/api/studentApi.js
+++ studentManagementFrontend/src/api/studentApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"http://localhost:8080/students"
+    baseURL:"https://urban-guide-r499wv4x6q5w3pv74-8082.app.github.dev/students"
 })
 
 export default api;
